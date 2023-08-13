72b743658e4aa481e7a963b44f48ee4f1e3eb341
Azabenathi Pupuma <pupuma.azabenathi@gmail.com>
2023-08-13 05:04:14 +0200

    onchanges hook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnChangesComponent } from './on-changes/on-changes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnChangesComponent,
   ],
   imports: [
     BrowserModule,
