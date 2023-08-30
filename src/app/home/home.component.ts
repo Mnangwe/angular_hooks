97f009db4e02e9d368142e059b187fbca8a48731
Azabenathi Pupuma <pupuma.azabenathi@gmail.com>
2023-08-30 18:54:42 +0200

    similarieties between observable and promise

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // PROMISE
+    const promise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve('Promise working!');
+      }, 1000)
+    })
+
+    promise.then(res => console.log(res));
+
+
+    // OBSERVABLE
+    const observable = new Observable(subscribe => {
+      setTimeout(() => {
+        subscribe.next('Observable working');
+        
+      }, 1000)
+    })
+
+    observable.subscribe(res => console.log(res))
   }
 
 }
